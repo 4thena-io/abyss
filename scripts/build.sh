fa2290747a16da7f672ab7d06ab3f6787b1c83e1
David Ramirez <david.ramirez@4thena.io>
2025-12-01 08:35:40 -0500

    🐛 fix no CGO enabled error

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ mkdir -p build
 
 for arch in amd64 arm64; do
     echo "Building $arch binary"
-    CGO_ENABLED=0 GOOS=linux GOARCH=$arch \
+    GOOS=linux GOARCH=$arch \
         go build -o ./build/app-$arch.out ./cmd/api
     if [ $? -ne 0 ]; then
         echo "Error: Build for $arch failed."
