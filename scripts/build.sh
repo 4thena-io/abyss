c75e7482efce432f386a856e66a10832ce2d862d
David Ramirez <david.ramirez@4thena.io>
2025-12-01 14:25:29 -0500

    💚 update xgo docker image

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,19 +4,11 @@ echo "Building the app with xgo"
 
 mkdir -p build
 
-if ! command -v xgo &> /dev/null; then
-    go install src.techknowlogick.com/xgo@latest
-fi
-
-# Build for both architectures with CGO support
 xgo -out app \
     -targets 'linux/amd64,linux/arm64' \
     -dest ./build \
+		-tags 'sqlite sqlite_unlock_notify' \
     -ldflags '-s -w' \
     ./cmd/api
-
-# Rename outputs to match your naming convention
-mv ./build/app-linux-amd64 ./build/app-amd64.out
-mv ./build/app-linux-arm64 ./build/app-arm64.out
 
 echo "Build Complete"
