51e0207a9252fe511918910b9bb7404b0777d42a
David Ramirez <ramirezmonroyd@gmail.com>
2026-01-12 00:42:41 -0500

    🔧 isolate the server into its own package

---
+++ internal/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"git.4thena.io/4thena/abys/internal/router"
+)
+
+type Server struct {
+	http *http.Server
+}
+
+type Config struct {
+	Host string
+	Port string
+}
+
+func New(cfg Config) *Server {
+	addr := cfg.Host + ":" + cfg.Port
+
+	return &Server{
+		http: &http.Server{
+			Addr:         addr,
+			Handler:      router.New(),
+			ReadTimeout:  15 * time.Second,
+			WriteTimeout: 15 * time.Second,
+			IdleTimeout:  60 * time.Second,
+		},
+	}
+}
+
+func (s *Server) Start() error {
+	slog.Info("server running", "addr", "http://"+s.http.Addr)
+	return s.http.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.http.Shutdown(ctx)
+}
